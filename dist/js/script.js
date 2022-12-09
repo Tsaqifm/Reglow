295a341099e6a8f4604af9026b9627af9172a1a9
Tsaqif <mushoddaqt@gmail.com>
2022-12-09 14:25:30 +0700

    almost done

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,81 @@
+// hamburger
+const hamburger = document.querySelector("#hamburger");
+const kotak = document.querySelector("#kotak");
+const kotak2 = document.querySelector("#kotak2");
+const x = document.querySelector("#x");
+//navMenu
+const navMenu = document.querySelector("#nav-menu");
+
+//navbarfixed
+window.onscroll = function () {
+  const header = document.querySelector("nav");
+  const fixednav = header.offsetTop;
+  if (window.pageYOffset > fixednav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
+
+hamburger.addEventListener("click", function () {
+  kotak.classList.toggle("hidden");
+  x.classList.toggle("hidden");
+  navMenu.classList.toggle("hidden");
+});
+
+// klik di luar hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != navMenu && e.target != hamburger && e.target != kotak && e.target != kotak2) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+    x.classList.add("hidden");
+    kotak.classList.remove("hidden");
+  }
+});
+
+//GET YEAR TO FOOTER(COPYRIGHT)
+(() => {
+  const date = new Date();
+  let year = date.getFullYear();
+  document.querySelector("#year").innerHTML = year;
+  return year;
+})();
+
+// ANIMATION COUNTER
+// window.onscroll = myScroll;
+// var counter = 0; // Global Variable
+// function myScroll() {
+//   if (counter == 0) {
+//     // if counter is 1, it will not execute
+//     if (window.pageYOffset > 3316) {
+//       // count animation
+//       function countTo() {
+//         let from = 340000;
+//         let to = 342178;
+//         let step = to > from ? 1 : -1;
+//         let interval = 1;
+
+//         if (from == to) {
+//           document.querySelector("#output").textContent = from;
+//           return;
+//         }
+
+//         let counter = setInterval(function () {
+//           from += step;
+//           document.querySelector("#output").textContent = from;
+
+//           if (from == to) {
+//             clearInterval(counter);
+//           }
+//         }, interval);
+//       }
+//       countTo();
+//       counter++; // increment the counter by 1, new value = 1
+//     }
+//   }
+// }
+
+// from libarary swiper.js
 var swiper = new Swiper(".mySwiper", {
   cssMode: true,
   loop: true,
@@ -16,72 +94,20 @@ var swiper = new Swiper(".mySwiper", {
   keyboard: true,
 });
 
-// hamburger
-const hamburger = document.querySelector("#hamburger");
-const kotak = document.querySelector("#kotak");
-const kotak2 = document.querySelector("#kotak2");
-const x = document.querySelector("#x");
-//navMenu
-const navMenu = document.querySelector("#nav-menu");
-
-//navbarfixed
-window.onscroll = function () {
-  const header = document.querySelector("nav");
-
-  const fixednav = header.offsetTop;
-
-  if (window.pageYOffset > fixednav) {
-    header.classList.add("navbar-fixed");
-  } else {
-    header.classList.remove("navbar-fixed");
-  }
-};
-
-window.onscroll = myScroll;
-var counter = 0; // Global Variable
-function myScroll() {
-  if (counter == 0) {
-    // if counter is 1, it will not execute
-    if (window.pageYOffset > 3316) {
-      // count animation
-      function countTo() {
-        let from = 340000;
-        let to = 342178;
-        let step = to > from ? 1 : -1;
-        let interval = 1;
-
-        if (from == to) {
-          document.querySelector("#output").textContent = from;
-          return;
-        }
-
-        let counter = setInterval(function () {
-          from += step;
-          document.querySelector("#output").textContent = from;
-
-          if (from == to) {
-            clearInterval(counter);
-          }
-        }, interval);
-      }
-      countTo();
-      counter++; // increment the counter by 1, new value = 1
-    }
-  }
-}
-
-hamburger.addEventListener("click", function () {
-  kotak.classList.toggle("hidden");
-  x.classList.toggle("hidden");
-  navMenu.classList.toggle("hidden");
-});
-
-// klik di luar hamburger
-window.addEventListener("click", function (e) {
-  if (e.target != navMenu && e.target != hamburger && e.target != kotak && e.target != kotak2) {
-    hamburger.classList.remove("hamburger-active");
-    navMenu.classList.add("hidden");
-    x.classList.add("hidden");
-    kotak.classList.remove("hidden");
-  }
+var swiper = new Swiper(".mySwiper2", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
